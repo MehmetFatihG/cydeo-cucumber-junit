e88a424d85c7c0cd9812288b20b06072b8c9ffdf
fatih <goksanfatih@gmail.com>
2022-07-01 18:42:47 +0300

    initial commit

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,52 @@
+package com.cydeo.utilities;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
+
+public class BrowserUtils {
+
+    //it accepts integer seconds and execute Thread.sleep
+    public static void sleep(int second){
+        second *=1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //it switches between the open pages, gets the given Url and verifies the expected title
+    public static void switchWindowAndVerify(String expectedInUrl, String expectedTitle){
+
+        Set<String> list = Driver.getDriver().getWindowHandles();
+        for(String each : list ){
+            Driver.getDriver().switchTo().window(each);
+            if(Driver.getDriver().getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+        }
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+    }
+
+    //it asserts if the expected title is true
+    public static void verifyExpectedTitle(String expectedTitle){
+
+        Assert.assertEquals(Driver.getDriver().getTitle(), expectedTitle);
+    }
+
+    //it waits explicitly for invisibility
+    public static void waitForInvisibilityOf(WebElement element){
+        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+
+}
