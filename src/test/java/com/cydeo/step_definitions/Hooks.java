b3ac27926f14cc6410c6ae29886665aef6998fe3
fatih <goksanfatih@gmail.com>
2022-07-01 19:39:10 +0300

    initial commit

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,15 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.Before;
+
+/*
+in this class we will be able to pass pre- & post- conditions to each scenario and each step
+ */
+public class Hooks {
+
+    @Before//import from io.cucumber.java not from junit
+    public void setupScenario(){
+
+    }
+
+}
