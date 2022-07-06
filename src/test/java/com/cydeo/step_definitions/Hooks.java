7898c6e8665f7230f6f6a9fb385ae24415c55af6
fatih <goksanfatih@gmail.com>
2022-07-07 00:07:12 +0300

    initial commit

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -10,12 +10,12 @@ in this class we will be able to pass pre- & post- conditions to each scenario a
  */
 public class Hooks {
 
-    @Before(order = 0)//import from io.cucumber.java not from junit
+    //@Before(order = 0)//import from io.cucumber.java not from junit
     public void setupScenario(){
         System.out.println("===Setting up browser using cucumber @Before");
     }
 
-    @Before (value = "@login", order = 1)
+    //@Before (value = "@login", order = 1)
     public void setupScenarioForSettings(){
         System.out.println("===This will only apply to scenarios with @login tag");
     }
@@ -33,12 +33,12 @@ public class Hooks {
         //System.out.println("===Scenario ended/ Take screenshot if failed");
     }
 
-    @BeforeStep
+    //@BeforeStep
     public void setupStep(){
         System.out.println("--------applying setup using @BeforeStep");
     }
 
-    @AfterStep
+    //@AfterStep
     public void afterStep(){
         System.out.println("--------applying teardown using @AfterStep");
     }
--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -13,7 +13,9 @@ public class Driver {
        We make WebDriver private, because we want to close access from outside the class.
        We make it static because we will use it in a static method.
         */
-    private static WebDriver driver;
+    //private static WebDriver driver;
+
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
     /*
    Creating a private constructor, so we are closing
@@ -24,32 +26,32 @@ public class Driver {
 
     public static WebDriver getDriver(){
 
-        if(driver == null){
+        if(driverPool.get() == null){
             String browserType = ConfigurationReader.getProperty("browser");
 
             switch(browserType){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
                 case "firefox":
-                    driver = new FirefoxDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    driverPool.get().manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
                     break;
             }
         }
 
-        return driver;
+        return driverPool.get();
     }
 
 
     public static void closeDriver(){
-        if(driver != null){
-            driver.quit();
-            driver = null;
+        if(driverPool.get() != null){
+            driverPool.get().quit();
+            driverPool.remove();
         }
     }
 
